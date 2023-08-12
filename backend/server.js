da8d14d770b4f0fce717aaf76460252ab0b705cd
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-12 19:14:13 +0530

    feat: added dot env

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 //routes
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const port = 5000;
+const port = process.env.SERVER_PORT || 5000;
 
 app.use(express.json());
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use('/api/v1/users', userRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hi From Server!' })
+  res.status(200).json({ message: 'Hi From Server!' })
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 })
