56a0b360807b6c8b70931aa6383f007ce0da0520
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-12 15:17:55 +0530

    init-frontend-setup

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# A MERN stack user authentication project
+
+This project is built using MongoDB, ExpressJS, ReactJS and NodeJS for providing basic user authentication for any MERN based projects.
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUp from '../../components/SignUp';
+import SignIn from '../../components/SignIn';
+
+const Home = () => {
+  return (
+    <>
+      <SignUp />
+      <SignIn />
+    </>
+  )
+}
+
+export default Home
