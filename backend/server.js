6249432b0a569c20004e461469ce5a8d9ea2bea5
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-13 19:37:47 +0530

    feat: added user model and code for creating user in db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+
+import connectDB from "./database/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
-
 dotenv.config({ path: "./config.env" });
+
+connectDB();
+
 const { SERVER_PORT, NODE_ENV } = process.env;
 
 const app = express();
 const port = SERVER_PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
