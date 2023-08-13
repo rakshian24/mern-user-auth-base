e7b91d6b6f67bda935609103a50f5b4b6c9dea68
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-13 23:14:01 +0530

    feat: added dashboard page - ui completed

---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { DashboardContainer, DashboardTitle } from './styles';
+
+const Dashboard = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return (
+    <DashboardContainer>
+      <DashboardTitle>
+        Welcome {userInfo?.name}!
+      </DashboardTitle>
+    </DashboardContainer>
+  )
+}
+
+export default Dashboard
---
+++ frontend/src/pages/dashboard/styles.js
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+
+export const DashboardContainer = styled.div`
+  padding-top: 0;
+
+  @media screen and (min-width: 1024px){
+    padding-top: 2rem;
+  }
+`;
+
+export const DashboardTitle = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 400;
+  
+  @media screen and (min-width: 501px) and (max-width: 1023px){
+    font-size: 3rem;
+  }
+
+  @media screen and (min-width: 1024px){
+    font-size: 4rem;
+  }
+`;
