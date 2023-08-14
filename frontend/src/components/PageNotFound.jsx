c187d3c7701d1b9c3dcc1c045c8c0bf8a877691c
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-08-15 00:23:44 +0530

    feat: added 404 page for client

---
+++ frontend/src/components/PageNotFound.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ReactComponent as PageNotFoundSvg } from './svgs/PageNotFoundSpaceMan.svg';
+import { styled } from 'styled-components';
+import { useWindowSize } from '../hooks/useWindowResize';
+import { FormButton } from '../common/styled-components/index.js';
+import { Link } from 'react-router-dom';
+
+const PageNotFoundHeader = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 4rem;
+  font-weight: 400;
+  text-align: center;
+  margin-bottom: 3rem;
+
+  @media screen and (min-width: 501px){
+    font-size: 3rem;
+  }
+`;
+
+const PageNotFound = () => {
+  const [screenWidth] = useWindowSize();
+
+  const getWidth = (width) => {
+    //Mobile screens
+    if(width <= 500){
+      return '100%'
+    }
+    //Tablet screens
+    else if(width >= 501 && width <=1023){
+      return '600px'
+    }
+    //Laptops and above screens
+    else{
+      return '900px'
+    }
+  }
+
+  return (
+    <div>
+      <div style={{ textAlign: 'center', marginBottom: "3rem" }}>
+        <PageNotFoundHeader>Sorry, page not found!</PageNotFoundHeader>
+        <Link to={'/dashboard'}>
+          <FormButton>
+            Go to HomePage
+          </FormButton>
+        </Link>
+      </div>
+      <PageNotFoundSvg width={getWidth(screenWidth)} />
+    </div>
+  )
+}
+
+export default PageNotFound
